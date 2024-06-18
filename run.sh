59c5a14cd72ffd200d35d676585562673ad95e5a
Nils Dagsson Moskopp <nils.moskopp@grandcentrix.net>
2024-06-18 15:33:29 +0200

    Create virtualenv in separate venv folder

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-. ./bin/activate
+. ./venv/bin/activate
 flake8 ./main.py
 mypy ./main.py
 ./main.py
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
-python3 -m venv .
-. ./bin/activate
+python3 -m venv venv
+. ./venv/bin/activate
 pip3 install -r requirements.txt
